63c37a407a95008888b1e5ad915da3e5227cf0e1
unknown <dwarszaw@udel.edu>
2020-02-28 17:42:52 -0500

    Fixed image and sprite paths again

--- src/scripts/scenes/preloadScene.ts
+++ src/scripts/scenes/preloadScene.ts
@@ -4,42 +4,42 @@ export default class PreloadScene extends Phaser.Scene {
   }
 
   preload() {
-    this.load.image("background", "/assets/images/background.png");
+    this.load.image("background", "assets/images/background.png");
     /*the background image is now ready to be used in the game
     because it is now placed in memory */
    // this.load.image("ship", "/src/assets/images/ship.png");
    //this.load.image("ship2", "/src/assets/images/ship2.png");
    //this.load.image("ship3", "/src/assets/images/ship3.png");
 
-   this.load.spritesheet("ship", "/assets/spritesheets/ship.png", {
+   this.load.spritesheet("ship", "assets/spritesheets/ship.png", {
      frameWidth: 16, //size of sprite's frame
      frameHeight: 16 
     });
-   this.load.spritesheet("ship2", "/assets/spritesheets/ship2.png", {
+   this.load.spritesheet("ship2", "assets/spritesheets/ship2.png", {
       frameWidth: 32, //size of sprite's frame
       frameHeight: 16 
     });
-    this.load.spritesheet("ship3", "/assets/spritesheets/ship3.png", {
+    this.load.spritesheet("ship3", "assets/spritesheets/ship3.png", {
       frameWidth: 32, //size of sprite's frame
       frameHeight: 32 
     });
-    this.load.spritesheet("explosion", "/assets/spritesheets/explosion.png", {
+    this.load.spritesheet("explosion", "assets/spritesheets/explosion.png", {
       frameWidth: 16, //size of sprite's frame
       frameHeight: 16 
     });
-    this.load.spritesheet("power-up", "/assets/spritesheets/power-up.png",{
+    this.load.spritesheet("power-up", "assets/spritesheets/power-up.png",{
       frameWidth: 16,
       frameHeight: 16
     });
-    this.load.spritesheet("player", "/assets/spritesheets/player.png",{
+    this.load.spritesheet("player", "assets/spritesheets/player.png",{
       frameWidth: 16,
       frameHeight: 24
     });
-    this.load.spritesheet("beam", "/assets/spritesheets/beam.png",{
+    this.load.spritesheet("beam", "assets/spritesheets/beam.png",{
       frameWidth: 16,
       frameHeight: 16
     });
-    this.load.bitmapFont("pixelFont", "/assets/font/font.png", "/assets/font/font.xml");
+    this.load.bitmapFont("pixelFont", "assets/font/font.png", "/assets/font/font.xml");
   }
 
   create() {
